eaf8cb0433ad1e2b276d6786d587cf83e68ef632
mathangspk <mathangspk@gmail.com>
2021-06-18 16:06:18 +0700

    change pass databse

--- config/keys.js
+++ config/keys.js
@@ -3,5 +3,5 @@ module.exports = {
     //mongoURI : 'mongodb://champaUser:champaUser@113.161.220.166:27017/manageYenSaoChamPa'
    //mongoURI : 'mongodb://localhost:27017/manageTool'
     //mongoURI:'mongodb://manage:!manage@128.199.82.173:27017/manageTool'
-    mongoURI:'mongodb://mqtt:!mqtt@128.199.82.173:27017/mqttJS'
+    mongoURI:'mongodb://mqtt:!mqtt18062021@128.199.82.173:27017/mqttJS'
 }
